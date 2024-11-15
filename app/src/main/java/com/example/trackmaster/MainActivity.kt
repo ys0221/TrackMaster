c87f0931d5038740d888521cd001e17d77bb4cf9
jyjbn1789 <jyjbn1789@gmail.com>
2024-11-15 17:49:22 +0900

    편의시설 추가

--- app/src/main/java/com/example/trackmaster/MainActivity.kt
+++ app/src/main/java/com/example/trackmaster/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.trackmaster
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -22,63 +23,91 @@ data class Edge(val destination: String, val time: Int, val distance: Int, val c
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val startStationInput = findViewById<EditText>(R.id.startStationInput)
         val endStationInput = findViewById<EditText>(R.id.endStationInput)
-        val searchButton = findViewById<Button>(R.id.searchButton)
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
 
         val stationList = readCSV(this)
         val graph = buildGraph(stationList)
 
-        // 두 경로 계산 후 데이터를 Intent에 추가하는 부분
-        searchButton.setOnClickListener {
+        calculateButton.setOnClickListener {
             val startStation = startStationInput.text.toString()
             val endStation = endStationInput.text.toString()
 
-            if (startStation.isNotBlank() && endStation.isNotBlank()) {
-                val minTransferResult = findLowestCostPathWithTransfers(graph, startStation, endStation)
-                val minCostResult = findLowestCostPath(graph, startStation, endStation)
+            if (startStation.isBlank() || endStation.isBlank()) {
+                Toast.makeText(this, "출발역과 도착역을 입력하세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
-                if (minTransferResult != null && minCostResult != null) {
-                    val intent = Intent(this, ResultActivity::class.java)
+            val minTransferResult = findLowestCostPathWithTransfers(graph, startStation, endStation)
+            val minCostResult = findLowestCostPath(graph, startStation, endStation)
+            val minTimeResult = findLowestTimePath(graph, startStation, endStation)
 
-                    // 최소 환승 경로 데이터 추가
-                    intent.putStringArrayListExtra("transferRoute", ArrayList(minTransferResult.route))
-                    intent.putIntegerArrayListExtra("transferDistance", ArrayList(minTransferResult.distances))
-                    intent.putIntegerArrayListExtra("transferCosts", ArrayList(minTransferResult.costs))
-                    intent.putIntegerArrayListExtra("transferTimes", ArrayList(minTransferResult.times))
-                    intent.putExtra("transferTransfers", minTransferResult.totalTransfers)
+            if (minTransferResult != null && minCostResult != null && minTimeResult != null) {
+                val resultText = """
+                    최소 환승 경로
+                    경로: ${minTransferResult.route.joinToString(" -> ")}
+                    비용: ${minTransferResult.costs.sum()} 원
+                    거리: ${minTransferResult.distances.sum()} m
+                    소요 시간: ${minTransferResult.times.sum()} 초
+                    
+                    혼잡도: 
+                    ${calculateCongestion(minTransferResult)}
 
-                    // 최소 비용 경로 데이터 추가
-                    intent.putStringArrayListExtra("costRoute", ArrayList(minCostResult.route))
-                    intent.putIntegerArrayListExtra("costDistance", ArrayList(minCostResult.distances))
-                    intent.putIntegerArrayListExtra("costs", ArrayList(minCostResult.costs))
-                    intent.putIntegerArrayListExtra("times", ArrayList(minCostResult.times))
-                    intent.putExtra("totalCost", minCostResult.totalCost)
-                    intent.putExtra("costTransfers", minCostResult.totalTransfers)
+                    최소 비용 경로
+                    경로: ${minCostResult.route.joinToString(" -> ")}
+                    비용: ${minCostResult.totalCost} 원
+                    거리: ${minCostResult.distances.sum()} m
+                    소요 시간: ${minCostResult.times.sum()} 초
+                    
+                    혼잡도: 
+                    ${calculateCongestion(minCostResult)}
 
-                    startActivity(intent)
-                } else {
-                    Toast.makeText(this, "경로를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+                    최소 시간 경로
+                    경로: ${minTimeResult.route.joinToString(" -> ")}
+                    비용: ${minTimeResult.totalCost} 원
+                    거리: ${minTimeResult.distances.sum()} m
+                    소요 시간: ${minTimeResult.times.sum()} 초
+                    
+                    혼잡도: 
+                    ${calculateCongestion(minTimeResult)}
+                """.trimIndent()
+
+                // ResultActivity로 이동하며 결과를 전달
+                val intent = Intent(this, ResultActivity::class.java).apply {
+                    putExtra("resultText", resultText)
                 }
+                startActivity(intent)
             } else {
-                Toast.makeText(this, "출발역과 도착역을 입력하세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "경로를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
 
+    private fun calculateCongestion(path: Path): String {
+        val lineCongestions = mutableMapOf<String, MutableList<Int>>()
+
+        path.route.forEach { station ->
+            val line = station.substring(0, 1) // 역 이름의 첫 글자가 호선 번호라고 가정
+            val congestion = Random().nextInt(100) + 1 // 1부터 100 사이의 랜덤 혼잡도
+            lineCongestions.computeIfAbsent(line) { mutableListOf() }.add(congestion)
+        }
+
+        // 호선별 평균 혼잡도 계산 후 줄바꿈으로 나열
+        return lineCongestions.entries.joinToString { "${it.key}호선 평균 혼잡도: ${it.value.sum() / it.value.size}%" }
     }
 
     private fun readCSV(context: Context): List<StationData> {
         val stationList = mutableListOf<StationData>()
-
         try {
             context.assets.open("stations.csv").use { inputStream ->
                 BufferedReader(InputStreamReader(inputStream)).use { reader ->
-                    reader.readLine() // 헤더 건너뜀
-
+                    reader.readLine()
                     reader.forEachLine { line ->
                         val tokens = line.split(",")
                         if (tokens.size == 5) {
@@ -97,28 +126,23 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             e.printStackTrace()
         }
-
         return stationList
     }
 
     private fun buildGraph(stationList: List<StationData>): Map<String, MutableList<Edge>> {
         val graph = mutableMapOf<String, MutableList<Edge>>()
-
         for (station in stationList) {
             graph.computeIfAbsent(station.출발역) { mutableListOf() }
                 .add(Edge(station.도착역, station.시간, station.거리, station.비용))
             graph.computeIfAbsent(station.도착역) { mutableListOf() }
                 .add(Edge(station.출발역, station.시간, station.거리, station.비용))
         }
-
         return graph
     }
 
-    // 최소 환승 경로 함수
     private fun findLowestCostPathWithTransfers(graph: Map<String, MutableList<Edge>>, start: String, end: String): Path? {
         val costs = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }
         val transfers = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }
-        val distances = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }
         val previousNodes = mutableMapOf<String, String?>()
         val routeDistances = mutableMapOf<String, List<Int>>()
         val routeCosts = mutableMapOf<String, List<Int>>()
@@ -126,13 +150,12 @@ class MainActivity : AppCompatActivity() {
 
         costs[start] = 0
         transfers[start] = 0
-        distances[start] = 0
         routeDistances[start] = listOf()
         routeCosts[start] = listOf()
         routeTimes[start] = listOf()
 
-        val priorityQueue = PriorityQueue<Triple<String, Int, Int>>(compareBy<Triple<String, Int, Int>> { it.third }.thenBy { distances.getValue(it.first) })
-        priorityQueue.add(Triple(start, 0, 0))  // (station, cost, transfers)
+        val priorityQueue = PriorityQueue<Triple<String, Int, Int>>(compareBy<Triple<String, Int, Int>> { it.third }.thenBy { costs.getValue(it.first) })
+        priorityQueue.add(Triple(start, 0, 0))
 
         while (priorityQueue.isNotEmpty()) {
             val (currentStation, currentCost, currentTransfers) = priorityQueue.poll()
@@ -153,15 +176,12 @@ class MainActivity : AppCompatActivity() {
             for (edge in graph[currentStation] ?: emptyList()) {
                 val newCost = currentCost + edge.cost
                 val newTransfers = if (currentStation.first() != edge.destination.first()) currentTransfers + 1 else currentTransfers
-                val newDistance = distances.getValue(currentStation) + edge.distance
 
-                // Update based on transfer count, and distance if transfer counts are equal
                 if (newTransfers < transfers.getValue(edge.destination) ||
-                    (newTransfers == transfers.getValue(edge.destination) && newDistance < distances.getValue(edge.destination))
+                    (newTransfers == transfers.getValue(edge.destination) && newCost < costs.getValue(edge.destination))
                 ) {
                     costs[edge.destination] = newCost
                     transfers[edge.destination] = newTransfers
-                    distances[edge.destination] = newDistance
                     previousNodes[edge.destination] = currentStation
                     routeDistances[edge.destination] = routeDistances[currentStation]!! + edge.distance
                     routeCosts[edge.destination] = routeCosts[currentStation]!! + edge.cost
@@ -171,37 +191,33 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        return null // If no path is found
+        return null
     }
 
-
-    // 최소 비용 경로 함수
     private fun findLowestCostPath(graph: Map<String, MutableList<Edge>>, start: String, end: String): Path? {
         val costs = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }
-        val transfers = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }
         val previousNodes = mutableMapOf<String, String?>()
         val routeDistances = mutableMapOf<String, List<Int>>()
         val routeCosts = mutableMapOf<String, List<Int>>()
         val routeTimes = mutableMapOf<String, List<Int>>()
 
         costs[start] = 0
-        transfers[start] = 0
         routeDistances[start] = listOf()
         routeCosts[start] = listOf()
         routeTimes[start] = listOf()
 
-        val priorityQueue = PriorityQueue<Triple<String, Int, Int>>(compareBy { it.second }) // 비용 기준 우선 정렬
-        priorityQueue.add(Triple(start, 0, 0))  // (역 이름, 비용, 환승 횟수)
+        val priorityQueue = PriorityQueue<Triple<String, Int, Int>>(compareBy { it.second })
+        priorityQueue.add(Triple(start, 0, 0))
 
         while (priorityQueue.isNotEmpty()) {
-            val (currentStation, currentCost, currentTransfers) = priorityQueue.poll()
+            val (currentStation, currentCost, _) = priorityQueue.poll()
 
             if (currentStation == end) {
                 val route = generateRoute(previousNodes, end)
                 return Path(
                     station = currentStation,
                     totalCost = routeCosts[end]!!.sum(),
-                    totalTransfers = currentTransfers,
+                    totalTransfers = 0,
                     route = route,
                     distances = routeDistances[end] ?: listOf(),
                     costs = routeCosts[end] ?: listOf(),
@@ -211,22 +227,67 @@ class MainActivity : AppCompatActivity() {
 
             for (edge in graph[currentStation] ?: emptyList()) {
                 val newCost = currentCost + edge.cost
-                val newTransfers = if (currentStation.first() != edge.destination.first()) currentTransfers + 1 else currentTransfers
 
-                // 최소 비용 기준으로 업데이트, 비용이 같으면 환승 횟수는 비교하지 않음
                 if (newCost < costs.getValue(edge.destination)) {
                     costs[edge.destination] = newCost
-                    transfers[edge.destination] = newTransfers
                     previousNodes[edge.destination] = currentStation
                     routeDistances[edge.destination] = routeDistances[currentStation]!! + edge.distance
                     routeCosts[edge.destination] = routeCosts[currentStation]!! + edge.cost
                     routeTimes[edge.destination] = routeTimes[currentStation]!! + edge.time
-                    priorityQueue.add(Triple(edge.destination, newCost, newTransfers))
+                    priorityQueue.add(Triple(edge.destination, newCost, 0))
                 }
             }
         }
 
-        return null // 경로를 찾지 못한 경우
+        return null
+    }
+
+    private fun findLowestTimePath(graph: Map<String, MutableList<Edge>>, start: String, end: String): Path? {
+        val times = mutableMapOf<String, Int>().withDefault { Int.MAX_VALUE }
+        val previousNodes = mutableMapOf<String, String?>()
+        val routeDistances = mutableMapOf<String, List<Int>>()
+        val routeCosts = mutableMapOf<String, List<Int>>()
+        val routeTimes = mutableMapOf<String, List<Int>>()
+
+        times[start] = 0
+        routeDistances[start] = listOf()
+        routeCosts[start] = listOf()
+        routeTimes[start] = listOf()
+
+        val priorityQueue = PriorityQueue<Triple<String, Int, Int>>(compareBy { it.second })
+        priorityQueue.add(Triple(start, 0, 0))
+
+        while (priorityQueue.isNotEmpty()) {
+            val (currentStation, currentTime, _) = priorityQueue.poll()
+
+            if (currentStation == end) {
+                val route = generateRoute(previousNodes, end)
+                return Path(
+                    station = currentStation,
+                    totalCost = routeCosts[end]!!.sum(),
+                    totalTransfers = 0,
+                    route = route,
+                    distances = routeDistances[end] ?: listOf(),
+                    costs = routeCosts[end] ?: listOf(),
+                    times = routeTimes[end] ?: listOf()
+                )
+            }
+
+            for (edge in graph[currentStation] ?: emptyList()) {
+                val newTime = currentTime + edge.time
+
+                if (newTime < times.getValue(edge.destination)) {
+                    times[edge.destination] = newTime
+                    previousNodes[edge.destination] = currentStation
+                    routeDistances[edge.destination] = routeDistances[currentStation]!! + edge.distance
+                    routeCosts[edge.destination] = routeCosts[currentStation]!! + edge.cost
+                    routeTimes[edge.destination] = routeTimes[currentStation]!! + edge.time
+                    priorityQueue.add(Triple(edge.destination, newTime, 0))
+                }
+            }
+        }
+
+        return null
     }
 
     private fun generateRoute(previousNodes: Map<String, String?>, end: String): List<String> {
--- app/src/main/java/com/example/trackmaster/ResultActivity.kt
+++ app/src/main/java/com/example/trackmaster/ResultActivity.kt
@@ -1,68 +1,24 @@
 package com.example.trackmaster
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        // 최소 비용 경로 데이터 받기
-        val costRoute = intent.getStringArrayListExtra("costRoute")
-        val costDistance = intent.getIntegerArrayListExtra("costDistance")
-        val costs = intent.getIntegerArrayListExtra("costs")
-        val times = intent.getIntegerArrayListExtra("times")
-        val costTransfers = intent.getIntExtra("costTransfers", -1)
-
-        // 최소 환승 경로 데이터 받기
-        val transferRoute = intent.getStringArrayListExtra("transferRoute")
-        val transferDistance = intent.getIntegerArrayListExtra("transferDistance")
-        val transferCosts = intent.getIntegerArrayListExtra("transferCosts")
-        val transferTimes = intent.getIntegerArrayListExtra("transferTimes")
-        val transferTransfers = intent.getIntExtra("transferTransfers", -1)
-
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
-
-        // 최소 비용 경로 총 비용 계산
-        val totalCost = costs?.sum() ?: -1
-
-        // 최소 환승 경로 총 비용 계산
-        val transferTotalCost = transferCosts?.sum() ?: -1
-
-        // 최소 비용 경로 표시
-        val costRouteString = buildString {
-            append("최소 비용 경로:\n")
-            costRoute?.forEachIndexed { index, station ->
-                append(station)
-                if (index < costDistance?.size ?: 0) {
-                    append(" (${costDistance?.get(index)}m, ${costs?.get(index)}원, ${times?.get(index)}초)")
-                    if (index < costRoute.size - 1) {
-                        append(" -> ")
-                    }
-                }
-            }
-            append("\n총 비용: $totalCost 원\n총 환승 횟수: $costTransfers 회\n")
+        val resultTextView = findViewById<TextView>(R.id.resultText)
+        val resultText = intent.getStringExtra("resultText")
+        val nearbyButton = findViewById<Button>(R.id.nearbyButton)
+        nearbyButton.setOnClickListener {
+            val intent = Intent(this, NearbySearchActivity::class.java)
+            startActivity(intent)
         }
 
-        // 최소 환승 경로 표시
-        val transferRouteString = buildString {
-            append("최소 환승 경로:\n")
-            transferRoute?.forEachIndexed { index, station ->
-                append(station)
-                if (index < transferDistance?.size ?: 0) {
-                    append(" (${transferDistance?.get(index)}m, ${transferCosts?.get(index)}원, ${transferTimes?.get(index)}초)")
-                    if (index < transferRoute.size - 1) {
-                        append(" -> ")
-                    }
-                }
-            }
-            append("\n총 비용: $transferTotalCost 원\n총 환승 횟수: $transferTransfers 회\n")
-        }
-
-        // 결과를 TextView에 설정
-        resultTextView.text = "$costRouteString\n$transferRouteString"
+        resultTextView.text = resultText ?: "결과가 없습니다."
     }
 }
